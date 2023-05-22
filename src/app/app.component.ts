191d742e92356cc98802971174790ea5966efdf0
remyrm <remy.marquis62121@gmail.com>
2023-05-22 12:18:12 +0200

    change tittle on app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularChallenge';
+  title = 'Bienvenue sur le site de Rémy 😊 !';
 }
